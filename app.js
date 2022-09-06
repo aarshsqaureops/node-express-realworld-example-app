186b6b0ca7bf053e90ddf830dfefd65421c59139
aarshsqaureops <109953812+aarshsqaureops@users.noreply.github.com>
2022-09-06 21:36:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ if (!isProduction) {
 if(isProduction){
   mongoose.connect(process.env.MONGODB_URI);
 } else {
-  mongoose.connect('mongodb://admin:password@10.0.3.80:27017,10.0.4.244:27017,10.0.4.164:27017/conduit?authSource=admin&replicaSet=rs0');
+  mongoose.connect('mongodb://admin:password@10.0.4.121:27017,10.0.4.109:27017,10.0.4.90:27017/conduit?authSource=admin&replicaSet=rs0');
   mongoose.set('debug', true);
 }
 
