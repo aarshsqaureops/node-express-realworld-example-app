2c4aca6474a0838e3fbdb7bb727bfd12de24307f
aarshsqaureops <109953812+aarshsqaureops@users.noreply.github.com>
2022-12-15 15:47:48 +0530

    Update start.sh

--- script/start.sh
+++ script/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd ~
+sudo chmod -R 777 /home/ubuntu/node-express-realworld-example-app
 cd /home/ubuntu/node-express-realworld-example-app
 npm install npm@latest
 npm install pm2@latest
