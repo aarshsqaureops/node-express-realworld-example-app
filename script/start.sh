6f366c32cf2ad0d8017282c9b647ecacc3255ce9
aarshsqaureops <109953812+aarshsqaureops@users.noreply.github.com>
2022-12-15 13:42:02 +0530

    Update start.sh

--- script/start.sh
+++ script/start.sh
@@ -2,4 +2,6 @@
 cd ~
 sudo chmod -R 777 /home/ubuntu/node-express-realworld-example-app
 cd /home/ubuntu/node-express-realworld-example-app
-sudo pm2 start app.js
+npm install npm@latest
+npm install pm2@latest
+pm2 start app.js
