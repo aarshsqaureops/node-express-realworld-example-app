5f748f5c22307bac9e045ff1e4adda21a852bf76
aarshsqaureops <109953812+aarshsqaureops@users.noreply.github.com>
2022-12-18 22:09:23 +0530

    Update stop.sh

--- script/stop.sh
+++ script/stop.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo "Nothing to stop!"
-#sudo systemctl restart codedeploy-agent
+echo "Stopping service if already running"
+cd ~
+cd /home/ubuntu/node-express-realworld-example-app
+sudo pm2 stop app.js
